9c982334374a752174a2abe7c81bc1bacf4d8931
aes421 <aes421@msstate.edu>
2015-08-23 20:32:24 -0500

    Starting point for chatbox server
    Names a port and declares the serversocket and socket

---
+++ ChatBox/src/chatbox/ChatBoxServer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chatbox;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Ashley E. Stallings
+ */
+public class ChatBoxServer {
+
+
+    public static void main(String[] args) {
+        int port = 8000;
+        DataInputStream in;
+        DataOutputStream out;
+        ServerSocket server;
+        Socket socket;
+        
+        
+    }
+    
+}
