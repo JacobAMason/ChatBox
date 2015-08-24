75a2682604cdba65c8a0efc45b1710ae1fc32062
aes421 <aes421@msstate.edu>
2015-08-23 21:04:52 -0500

    First test for server.  Makes connections then waits for a double from client
    Sets up the server, socket, input and output streams.  Once connection
    is made it reads a double from the client and prints it.

--- ChatBox/src/chatbox/ChatBoxServer.java
+++ ChatBox/src/chatbox/ChatBoxServer.java
@@ -8,8 +8,11 @@ package chatbox;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,6 +27,27 @@ public class ChatBoxServer {
         DataOutputStream out;
         ServerSocket server;
         Socket socket;
+        
+        
+        try {
+            server = new ServerSocket(port);
+            socket = server.accept();
+            
+            
+            
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            
+            
+            double num;
+            
+            num = in.readDouble();
+            
+            System.out.println(num);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ChatBoxServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
     }
